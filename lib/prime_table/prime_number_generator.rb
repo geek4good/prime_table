d7fbff60693d92a206dd8b9518fa29742821e119
Lucas Mbiwe <lucas.mbiwe@mendeley.com>
2017-01-24 19:10:21 +0000

    First passing version of the PrimeNumberGenerator
    * compare fractions a number can generate to a set of prime fractions
    * optimisation: only do this for odd numbers

--- lib/prime_table/prime_number_generator.rb
+++ lib/prime_table/prime_number_generator.rb
@@ -1,4 +1,37 @@
+require "set"
+
 module PrimeTable
   class PrimeNumberGenerator
+    def self.take(n)
+      generate_primes(n)
+      primes.take(n)
+    end
+
+    private
+
+    def self.generate_primes(up_to)
+      while primes.size < up_to
+        @num ||= primes[-1]
+        @num += 2
+        fractions = Set.new(1.upto(@num - 1).map { |i| Rational(i, @num) })
+
+        if prime_fractions.disjoint?(fractions)
+          prime_fractions.merge(fractions)
+          primes << @num
+        end
+      end
+    end
+
+    def self.primes
+      @primes ||= [2, 3]
+    end
+
+    def self.prime_fractions
+      @prime_fractions ||= Set.new([Rational(1, 2), Rational(1, 3), Rational(2, 3)])
+    end
+
+    def self.last_checked_num
+      @last_checked_num ||= primes[-1]
+    end
   end
 end
