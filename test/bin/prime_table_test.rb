a3347e8473ef71ac4329d9759013e2b98991621d
Lucas Mbiwe <lucas.mbiwe@mendeley.com>
2017-01-24 11:33:07 +0000

    Create (failing) end-to-end test
    * start testing from the outside in by stating the desired outcome
    * create stub executable 'bin/prime_table'

---
+++ test/bin/prime_table_test.rb
@@ -0,0 +1,20 @@
+require "test_helper"
+require "open3"
+
+describe "prime_table" do
+  let(:project_root) { File.expand_path("../../..", __FILE__) }
+  let(:bin) { File.join(project_root, "bin", "prime_table") }
+
+  describe "`bin/prime_table 3`" do
+    it "prints a multiplication table of the first three prime numbers" do
+      output, _ = Open3.capture2(bin)
+      assert_equal <<PRIME_TABLE, output
+|      |    2 |    3 |    5 |
+|    2 |    4 |    6 |   10 |
+|    3 |    6 |    9 |   15 |
+|    5 |   10 |   15 |   25 |
+
+PRIME_TABLE
+    end
+  end
+end
