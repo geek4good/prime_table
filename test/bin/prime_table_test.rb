1b8d563b6098b4729d68b9ee0159dcd81ec7ba1a
Lucas Mbiwe <lucas.mbiwe@mendeley.com>
2017-01-24 12:07:27 +0000

    Add (failing) test for `bin/prime_table 5`

--- test/bin/prime_table_test.rb
+++ test/bin/prime_table_test.rb
@@ -17,4 +17,19 @@ describe "prime_table" do
 PRIME_TABLE
     end
   end
+ 
+  describe "`bin/prime_table 5`" do
+    it "prints a multiplication table of the first three prime numbers" do
+      output, _ = Open3.capture2(bin, "5")
+      assert_equal <<PRIME_TABLE, output
+|      |    2 |    3 |    5 |    7 |   11 |
+|    2 |    4 |    6 |   10 |   14 |   22 |
+|    3 |    6 |    9 |   15 |   21 |   33 |
+|    5 |   10 |   15 |   25 |   35 |   55 |
+|    7 |   14 |   21 |   35 |   49 |   77 |
+|   11 |   22 |   15 |   25 |   77 |  121 |
+
+PRIME_TABLE
+    end
+  end
 end
